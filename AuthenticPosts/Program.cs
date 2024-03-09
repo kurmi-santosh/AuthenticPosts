a668fcaa04f25359a8f7ab52193e97453ebfc8e3
Kurmi <kurmi.gorle@cerner.com>
2024-03-09 10:34:25 +0530

    Basics in setting a cookie without any protection

--- AuthenticPosts/Program.cs
+++ AuthenticPosts/Program.cs
@@ -1,115 +1,26 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using AuthenticPosts.Data;
-using AuthenticPosts.Services;
-using AuthenticPosts.Config;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using Microsoft.OpenApi.Models;
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-
-builder.Services.AddDbContext<DataContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<DataContext>();
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddScoped<IPostService, PostService>();
-
-var jwtSettings = new JWTSettings();
-builder.Configuration.Bind(nameof(JWTSettings), jwtSettings);
-builder.Services.AddSingleton(jwtSettings);
-
-builder.Services.AddAuthentication(config =>
-{
-    config.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-    config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(x =>
-{
-    x.SaveToken = true;
-    x.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtSettings.Secret)),
-        ValidateIssuer = false,
-        ValidateAudience = false,
-        RequireExpirationTime = false,
-        ValidateLifetime = true
-    };
-});
-
-builder.Services.AddSwaggerGen(x =>
-{
-    x.SwaggerDoc("v1", new OpenApiInfo { Title = "Posts API", Version = "v1" });
-
-    x.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description = "JWT Auth header using the bearer scheme",
-        Name = "Authorization",
-        In= ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey
-    });
-
-    x.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme {
-                Reference = new OpenApiReference{
-                    Id = "Bearer", //The name of the previously defined security scheme.
-                    Type = ReferenceType.SecurityScheme
-                }
-            }, new List<string>()
-        }
-    });
-});
+﻿var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.MapGet("/username", (HttpContext ctx) =>
 {
-    app.UseMigrationsEndPoint();
-}
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-var swaggerOptions = new SwaggerOptions();
-builder.Configuration.GetSection(nameof(SwaggerOptions)).Bind(swaggerOptions);
-
-app.UseSwagger(option =>
-{
-    option.RouteTemplate = swaggerOptions.JSONRoute;
+    var authCookie = ctx.Request.Headers.Cookie.FirstOrDefault(x => x!=null && x.StartsWith("auth="));
+    var payload = authCookie?.Split("=").Last();
+    if (payload!=null)
+    {
+        var parts = payload.Split(":");
+        var key = parts[0];
+        var value = parts[1];
+        return value;
+    }
+    return null;
 });
 
-app.UseSwaggerUI(option =>
+app.MapGet("/login", (HttpContext ctx) =>
 {
-    option.SwaggerEndpoint(swaggerOptions.UIEndpoint, "v1");
+    ctx.Response.Headers["set-cookie"] = "auth=usr:kurmi";
+    return "ok";
 });
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthentication();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
 
 app.Run();
 
