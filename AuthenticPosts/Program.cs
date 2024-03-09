4d2799657b9a729d46bf447a8260e1cce33d9394
Kurmi <kurmi.gorle@cerner.com>
2024-03-09 12:25:01 +0530

    Basic Authorization

--- AuthenticPosts/Program.cs
+++ AuthenticPosts/Program.cs
@@ -16,11 +16,25 @@ app.MapGet("/username", (HttpContext ctx) =>
     return ctx.User.FindFirst("usr")?.Value;
 });
 
+app.MapGet("/swedan", (HttpContext ctx) =>
+{
+    if(!ctx.User.Identities.Any(x => x.AuthenticationType == AuthScheme))
+    {
+        ctx.Response.StatusCode = 401;
+        return "Un-Authenticated";
+    }
+    if(ctx.User.HasClaim("nation", "swedan"))
+    {
+        return "allowed";
+    }
+    return "Un-Authorized";
+});
+
 app.MapGet("/login", async (HttpContext ctx) =>
 {
     var claims = new List<Claim>();
     claims.Add(new Claim("usr", "naidu"));
-
+    claims.Add(new Claim("nation", "swedan"));
     var identity = new ClaimsIdentity(claims, AuthScheme);
     var userCP = new ClaimsPrincipal(identity);
 
