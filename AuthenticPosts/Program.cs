d00a75c8559c6c3d7414d1f40740e472ca5dc492
Kurmi <kurmi.gorle@cerner.com>
2024-03-10 14:25:35 +0530

    Posts service config with Auth

--- AuthenticPosts/Program.cs
+++ AuthenticPosts/Program.cs
@@ -1,50 +1,116 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-
-const string AuthScheme = "cookie";
+using AuthenticPosts.Data;
+using AuthenticPosts.Services;
+using AuthenticPosts.Config;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddAuthentication(AuthScheme)
-       .AddCookie(AuthScheme);
 
-// Add Microsoft.EntityFrameworkCore.InMemory
-builder.Services.AddDbContext<IdentityDbContext>(builder => builder.UseInMemoryDatabase("my_db"));
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<DataContext>();
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<IPostService, PostService>();
+
+var jwtSettings = new JWTSettings();
+builder.Configuration.Bind(nameof(JWTSettings), jwtSettings);
+builder.Services.AddSingleton(jwtSettings);
+
+builder.Services.AddAuthentication(config =>
 {
-    options.User.RequireUniqueEmail = false;
+    config.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(x =>
+{
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtSettings.Secret)),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        RequireExpirationTime = false,
+        ValidateLifetime = true
+    };
+});
 
-    options.Password.RequireDigit = false;
-    options.Password.RequiredLength = 4;
-    options.Password.RequireLowercase = false;
-    options.Password.RequireUppercase = false;
-    options.Password.RequireNonAlphanumeric = false;
-}).AddEntityFrameworkStores<IdentityDbContext>()
-  .AddDefaultTokenProviders();
+builder.Services.AddSwaggerGen(x =>
+{
+    x.SwaggerDoc("v1", new OpenApiInfo { Title = "Posts API", Version = "v1" });
 
-builder.Services.AddAuthorization();
+    x.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Auth header using the bearer scheme",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey
+    });
 
-builder.Services.AddControllers();
+    x.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference{
+                    Id = "Bearer", //The name of the previously defined security scheme.
+                    Type = ReferenceType.SecurityScheme
+                }
+            }, new List<string>()
+        }
+    });
+});
+
 var app = builder.Build();
 
-// Let's create an admin role and add user to admin roles list
-using (var scope = app.Services.CreateScope()) {
-
-    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-    await roleManager.CreateAsync(new IdentityRole() { Name = "admin" });
-
-    var usermanager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
-   
-
-    var user = new IdentityUser() { UserName = "test@gmail.com", Email = "test@gmail.com" };
-    await usermanager.CreateAsync(user, password: "test");
-
-    await usermanager.AddToRoleAsync(user, "admin");
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+var swaggerOptions = new SwaggerOptions();
+builder.Configuration.GetSection(nameof(SwaggerOptions)).Bind(swaggerOptions);
 
-app.MapControllers();
+app.UseSwagger(option =>
+{
+    option.RouteTemplate = swaggerOptions.JSONRoute;
+});
+
+app.UseSwaggerUI(option =>
+{
+    option.SwaggerEndpoint(swaggerOptions.UIEndpoint, "v1");
+});
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+app.UseAuthentication();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 
 app.Run();
+
